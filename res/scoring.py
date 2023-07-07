21ce01ff9b10c8bc8eceab5be5b014fe19720939
Conway <yawnoc@pm.me>
2023-07-07 23:48:14 +0800

    Create res/scoring.py

---
+++ res/scoring.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+# scoring.py
+
+Generate `scores/score-table.cmdr` used by `scores/index.cmd`.
+
+To be called by `./build` in the root directory,
+after mahjong-scorer generates `scores.txt.tsv`
+but before conway-markdown generates `scores/index.html`.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()

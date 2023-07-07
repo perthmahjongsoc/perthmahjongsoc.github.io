d2e3112e3e7327832570b3ea6bff95d064a4d888
Conway <yawnoc@pm.me>
2023-07-07 23:21:19 +0800

    Add 404 page

---
+++ 404.cmd
@@ -0,0 +1,19 @@
+< /res/common.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: BEFORE #boilerplate-properties
+* %title --> Page not found
+
+%%%
+
+%header-nav
+
+
+# 404: Page not found
+
+--
+**The requested page was not found.**
+--
+--
+[Return to the home page.](/)
+--

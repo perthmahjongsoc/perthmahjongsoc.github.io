ccccfa41d3d0eee55ccdef5a046e81dc66893c9e
Conway <yawnoc@pm.me>
2023-07-07 23:34:53 +0800

    Add sitemap script

---
+++ sitemap.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""
+# sitemap.py
+
+Generate `sitemap.txt`.
+"""
+
+import os
+import re
+
+
+def to_canonical_url(file_name):
+    url = file_name
+
+    url = re.sub(r'\\', '/', url)
+    url = re.sub('/index[.]html$', '/', url)
+    url = re.sub('[.]html$', '', url)
+    url = re.sub('^[.]', 'https://example.com', url)  # TODO
+
+    return url
+
+
+def main():
+    urls = [
+        to_canonical_url(os.path.join(path, file_name))
+        for path, _, file_names in os.walk('.')
+        for file_name in file_names
+        if file_name.endswith('.html')
+        if file_name not in ['404.html']
+    ]
+    with open('sitemap.txt', 'w', encoding='utf-8') as sitemap_file:
+        for url in sorted(urls):
+            sitemap_file.write(f'{url}\n')
+
+
+if __name__ == '__main__':
+    main()

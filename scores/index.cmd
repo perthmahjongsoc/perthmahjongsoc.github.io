5202c98ce4c42ddd36fdac7455f03f8ed42dd8c8
Conway <yawnoc@pm.me>
2023-08-20 16:41:16 +0800

    Add latest meet table

--- scores/index.cmd
+++ scores/index.cmd
@@ -1,10 +1,11 @@
 < /res/common.cmdr
 < /scores/all-time.cmdr
+< /scores/latest.cmdr
 
 OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: BEFORE #boilerplate-properties
 * %title --> Scores
-* %date-modified --> 2023-07-09
+* %date-modified --> 2023-08-20
 
 %%%
 
@@ -18,6 +19,12 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 Compiled from [raw scores] using [`mahjongscore.py`].
 Rows are sorted by Net Score Per Game.
 --
+
+### Latest meet
+
+%%score-table(latest)
+
+### All time
 
 %%score-table(all-time)
 

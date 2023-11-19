1749b98042a01bfe00f802cd5737fb136f3675b9
Conway <yawnoc@pm.me>
2023-11-19 18:59:01 +0800

    Create /guide/

---
+++ guide/index.cmd
@@ -0,0 +1,19 @@
+< /res/common.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: BEFORE #boilerplate-properties
+* %title --> Guide
+* %date-modified --> 2023-11-19
+
+%%%
+
+
+# Guide
+
+--
+This reference guide is based on Hong Kong gameplay.
+--
+--
+There is no such thing as an offical version of mahjong.
+The game should be played according to player consensus.
+--

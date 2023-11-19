aa083df73bc782152daacecaeb9c62deb0e8642c
Conway <yawnoc@pm.me>
2023-11-19 20:55:44 +0800

    Debloat scores page

--- scores/index.cmd
+++ scores/index.cmd
@@ -12,29 +12,20 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 
 # Perth Mahjong Society Scores
 
-
-##{#compiled} Compiled scores
-
 --
-Compiled from [raw scores] using [`mahjongscore.py`].
+Compiled from [`scores.txt`] using [`mahjongscore.py`].
 Rows are sorted by Net Score Per Game.
 --
 
-###{#latest} Latest meet (%date-latest)
+
+##{#latest} Latest meet (%date-latest)
 
 %%score-table(latest)
 
-###{#all-time} All time
+
+##{#all-time} All time
 
 %%score-table(all-time)
 
-
-##{#raw} Raw scores
-
---
-See [`scores.txt`].
---
-
 [`mahjongscore.py`]: https://github.com/yawnoc/mahjong-scorer
-[raw scores]: #raw
 [`scores.txt`]: scores.txt

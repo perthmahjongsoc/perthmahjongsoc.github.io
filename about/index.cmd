d538f38d0cc35c47dcab8c3481d6502964c0fb34
Conway <yawnoc@pm.me>
2023-11-19 21:03:04 +0800

    Add heading permalinks

--- about/index.cmd
+++ about/index.cmd
@@ -3,7 +3,7 @@
 OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: BEFORE #boilerplate-properties
 * %title --> About
-* %date-modified --> 2023-07-09
+* %date-modified --> 2023-11-19
 
 %%%
 
@@ -16,7 +16,7 @@ from a group chat into an actual gathering.
 --
 
 
-## Established 2023-06-25
+##{#established} Established 2023-06-25
 
 --
 Whereas certain people of Perth thought it good

36c1026b4cb559d0d56fe79c67f17cc55d56bc56
Conway <yawnoc@pm.me>
2023-11-19 22:21:37 +0800

    Add Tiles section text

--- guide/index.cmd
+++ guide/index.cmd
@@ -21,7 +21,55 @@ The game should be played according to player consensus.
 
 ##{#concepts} Concepts
 
-###{#suits} Suits
+###{#tiles} Tiles
+
+#### Numbered suits
+
+======
+- 筒子 _t(')ung~tsz'_ = Tubes [circles, balls]
+  ====
+  - [one through nine]
+  ====
+
+- 索子 _sok~tsz'_ = Rods [bamboos, sticks]
+  ====
+  - [one through nine]
+  ====
+
+- 萬子 _m(a/)n~tsz'_ = Myriads [characters]
+  ====
+  - [one through nine]
+  ====
+======
+
+#### Unnumbered suits
+
+======
+- 番子 _f(a/)n~tsz'_ = Honours
+  ====
+  - 四風／四喜 = Four Winds / Four Joys [directions]
+    ==
+    - 東 = East
+    - 南 = South
+    - 西 = West
+    - 北 = North
+    ==
+
+  - 三元 = Three Duxes [dragons]
+    ==
+    - 紅中 = Red-Centre [red]
+    - 發財 = Prosper-Wealth [green]
+    - 白板 = White-Board [white]
+    ==
+
+  - 花 = Flowers
+    ==
+    - 四花 = Four Flora
+    - 四季 = Four Seasons
+    ==
+  ====
+======
+
 
 ###{#melds} Melds
 

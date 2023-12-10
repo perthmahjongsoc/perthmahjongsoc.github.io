f1afa0d6b0e3b04a0ee1690d0ebbbe724d5888ef
Conway <yawnoc@pm.me>
2023-12-10 19:03:15 +0800

    Add 2023-12-10 scores

--- scores/index.cmd
+++ scores/index.cmd
@@ -5,7 +5,7 @@
 OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: BEFORE #boilerplate-properties
 * %title --> Scores
-* %date-modified --> 2023-11-19
+* %date-modified --> 2023-12-10
 
 %%%
 
